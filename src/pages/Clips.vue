8faea6513e4faf3b640ceb748fd79fa2b125c25c
Pavmois <58141080+Pavmois@users.noreply.github.com>
2022-12-07 16:11:26 +0300

    New Vid

--- src/pages/Clips.vue
+++ src/pages/Clips.vue
@@ -2,6 +2,15 @@
   <div class="clips">
     <div class="clips-wrapper">
       
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
+      <video class="clip" src="@/assets/video/osc.mp4" controls></video>
     </div>
   </div>
 </template>
@@ -21,7 +30,20 @@ export default {
     overflow: scroll;
     -ms-overflow-style: none;
     scrollbar-width: none;
-    
+    .clips-wrapper {
+      display: flex;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      width: 80%;
+      margin: auto;
+      margin-top: 50px;
+      .clip {
+        width: 250px;
+        height: 150px;
+        padding: 10px;
+        margin-top: 10px;
+      }
+    }
   }
 
   .clips::-webkit-scrollbar {
